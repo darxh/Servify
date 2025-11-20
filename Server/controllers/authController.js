3f46f37bc169483d4f3af3ba6e86a838b4090f2d
darxh <darshanyavagal@gmail.com>
2025-11-20 20:40:27 +0530

    feat(auth): add register logic to auth controller

---
+++ Server/controllers/authController.js
@@ -0,0 +1,39 @@
+const User = require("../models/userModel");
+const generateToken = require("../utils/generateToken");
+
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password, role } = req.body;
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+      return res.status(400).json({ message: "user already exists" });
+    }
+
+    const user = await User.create({
+      name,
+      email,
+      password,
+      role,
+    });
+
+    if (user) {
+      const accessToken = generateToken(res, user._id);
+
+      res.status(201).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+        accessToken: accessToken,
+      });
+    } else {
+      res.status(500).json({ message: "invalid user data" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = registerUser;
