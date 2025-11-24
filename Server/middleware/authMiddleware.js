258f9e648c23ef2fe75cd893e566d4dce3eabc86
darxh <darshanyavagal@gmail.com>
2025-11-24 16:16:10 +0530

    feat(middleware): add protect and authorize middleware

---
+++ Server/middleware/authMiddleware.js
@@ -0,0 +1,42 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/userModel");
+
+const protect = async (req, res, next) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
+
+      const decoded = jwt.verify(token, process.env.JWT_ACCESS_SECRET);
+
+      req.user = await User.findById(decoded.userId).select("-password");
+
+      next();
+    } catch (error) {
+      console.error(error);
+      res.status(401).json({ message: "Not authorized ,token failed" });
+    }
+  }
+
+  if (!token) {
+    res.status(401).json({ message: "Not authorized ,no failed" });
+  }
+};
+
+const authorize = () => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return res.status(500).json({
+        message:
+          "User role '${req.user.role}' is not authorized to access this route",
+      });
+    }
+    next();
+  };
+};
+
+module.exports = { protect, authorize };
