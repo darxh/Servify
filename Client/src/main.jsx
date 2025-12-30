2120d981bfd7e304493789846aafd3f17996ee1b
darxh <darshanyavagal@gmail.com>
2025-12-30 23:23:27 +0530

    feat(client): wrap application in auth provider to enable global state

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -5,3 +5,11 @@ import { AuthProvider } from './context/AuthContext'; // 1. Import the Provider
 import router from './router.jsx';
 import './index.css';
 
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    {/* 2. Wrap the Router with AuthProvider */}
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+  </StrictMode>,
+);
