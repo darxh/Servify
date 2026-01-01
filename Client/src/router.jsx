8aa2a7856af42686d76d8c3ed4ffacd5fffdda81
darxh <darshanyavagal@gmail.com>
2026-01-01 22:58:40 +0530

    feat(client): connect login route to actual login page component

--- Client/src/router.jsx
+++ Client/src/router.jsx
@@ -1,41 +1,31 @@
 import { createBrowserRouter, Navigate } from "react-router-dom";
 import AuthLayout from "./layouts/AuthLayout";
 import MainLayout from "./layouts/MainLayout";
+import LoginPage from "./pages/auth/LoginPage"; // 1. Import the Real Page
 
-//Temporary Placeholders
-const LoginPlaceholder = () => <h1 className="text-2xl">Login Page</h1>;
 const RegisterPlaceholder = () => <h1 className="text-2xl">Register Page</h1>;
 const DashboardPlaceholder = () => <h1 className="text-2xl">Dashboard Home</h1>;
-const SigupPlaceholder = () => <h1 className="text-2x1">SingUp</h1>;
 
-//Router Definition
 const router = createBrowserRouter([
   {
-    // Root Redirect
     path: "/",
     element: <Navigate to="/dashboard" replace />,
   },
   {
-    //Auth Routes Group
     path: "/auth",
     element: <AuthLayout />,
     children: [
       {
         path: "login",
-        element: <LoginPlaceholder />,
+        element: <LoginPage />,
       },
       {
         path: "register",
         element: <RegisterPlaceholder />,
       },
-      {
-        path: "signup",
-        element: <SigupPlaceholder />,
-      },
     ],
   },
   {
-    //Dashboard Routes Group
     path: "/dashboard",
     element: <MainLayout />,
     children: [
