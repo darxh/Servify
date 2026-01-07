97e0b7b3d56a6983600fe6616f3c00eb4945ac97
darxh <darshanyavagal@gmail.com>
2026-01-07 21:39:42 +0530

    feat(client): register service details route

--- Client/src/router.jsx
+++ Client/src/router.jsx
@@ -3,7 +3,8 @@ import AuthLayout from "./layouts/AuthLayout";
 import MainLayout from "./layouts/MainLayout";
 import LoginPage from "./pages/auth/LoginPage";
 import RegisterPage from "./pages/auth/RegisterPage";
-import HomePage from "./pages/home/HomePage";  
+import HomePage from "./pages/home/HomePage";
+import ServiceDetailsPage from "./pages/services/ServiceDetailsPage"; // 1. Import the new page
 
 const DashboardPlaceholder = () => <h1 className="text-2xl">Dashboard Home</h1>;
 
@@ -11,6 +12,10 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <HomePage />,
+  },
+  {
+    path: "/services/:id", 
+    element: <ServiceDetailsPage />,
   },
   {
     path: "/auth",
