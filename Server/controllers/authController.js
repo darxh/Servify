efc988df4886f80934386dd5742a89de273ac5ea
darxh <darshanyavagal@gmail.com>
2026-01-18 17:21:50 +0530

    feat(server): add update profile endpoint

--- Server/controllers/authController.js
+++ Server/controllers/authController.js
@@ -1,5 +1,6 @@
 const User = require("../models/userModel");
 const generateToken = require("../utils/generateToken");
+const bcrypt = require("bcryptjs");
 
 const registerUser = async (req, res) => {
   try {
@@ -70,4 +71,31 @@ const getMe = async (req, res) => {
   }
 };
 
-module.exports = { registerUser, loginUser, getMe };
+const updateUserProfile = async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id);
+
+    if (user) {
+      user.name = req.body.name || user.name;
+      user.email = req.body.email || user.email;
+      if (req.body.password) {
+        user.password = req.body.password;
+      }
+
+      const updatedUser = await user.save();
+
+      res.json({
+        _id: updatedUser._id,
+        name: updatedUser.name,
+        email: updatedUser.email,
+        role: updatedUser.role,
+      });
+    } else {
+      res.status(404).json({ message: "User not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = { registerUser, loginUser, getMe, updateUserProfile, };
--- Server/routes/authRoutes.js
+++ Server/routes/authRoutes.js
@@ -5,6 +5,7 @@ const {
   registerUser,
   loginUser,
   getMe,
+  updateUserProfile,
 } = require("../controllers/authController");
 const { protect } = require("../middleware/authMiddleware");
 
@@ -12,5 +13,6 @@ router.post("/register", registerUser);
 router.post("/login", loginUser);
 
 router.get("/me", protect, getMe);
+router.put("/profile", protect, updateUserProfile);
 
 module.exports = router;
