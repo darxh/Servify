bede3609a89a01bba2c2433f8482be6ab3f04204
darxh <darshanyavagal@gmail.com>
2026-01-30 18:25:03 +0530

    feat(client):adding the logo and adding detaild gitignore and env.example files for both client and server

--- Client/src/App.jsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div className="min-h-screen bg-slate-900 flex items-center justify-center">
-      <h1 className="text-3xl font-bold text-blue-600">Hello Servify</h1>
-    </div>
-  );
-}
-
-export default App;
