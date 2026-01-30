99e84902ea791d173f4ee7080b4695d22ac307af
darxh <darshanyavagal@gmail.com>
2026-01-30 20:13:36 +0530

    updating the CORS for server

--- Server/server.js
+++ Server/server.js
@@ -22,11 +22,10 @@ app.use(cookieParser());
 
 // Server/server.js
 const corsOptions = {
-  origin: [
-    'http://localhost:5173', 
-    'https://servify-olive.vercel.app/'
-  ],
+  origin: process.env.CLIENT_URL || "http://localhost:5173",
   credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
 };
 app.use(cors(corsOptions));
 
